6dad58cb80d19459cb4ace5016ce0e5c0e43015f
Payton Jewell <paytontaylor96@gmail.com>
2020-09-15 12:39:17 -0500

    Updates

---
+++ precache-manifest.d44309e7ce5f778014889ea4d5864281.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3351ef287782483f34126a0e522f850",
+    "url": "/ReactPortfolioTemplate/index.html"
+  },
+  {
+    "revision": "a8f5c2daf33bd300d726",
+    "url": "/ReactPortfolioTemplate/static/css/main.b527fb99.chunk.css"
+  },
+  {
+    "revision": "e8f7bb38e1c78eae2b0a",
+    "url": "/ReactPortfolioTemplate/static/js/2.cdfb0668.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ReactPortfolioTemplate/static/js/2.cdfb0668.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8f5c2daf33bd300d726",
+    "url": "/ReactPortfolioTemplate/static/js/main.493457d4.chunk.js"
+  },
+  {
+    "revision": "89c4acd89fd006b267b1",
+    "url": "/ReactPortfolioTemplate/static/js/runtime-main.e90dc476.js"
+  },
+  {
+    "revision": "456d27d30afacf5ef9c2d04e25452c99",
+    "url": "/ReactPortfolioTemplate/static/media/mock1.456d27d3.png"
+  },
+  {
+    "revision": "37e11540fe7357463bd40d3effb35da1",
+    "url": "/ReactPortfolioTemplate/static/media/mock2.37e11540.png"
+  },
+  {
+    "revision": "6a736fcd570ebcf9699f8d5e9f949f60",
+    "url": "/ReactPortfolioTemplate/static/media/mock3.6a736fcd.png"
+  },
+  {
+    "revision": "bd6a383d15853619853b9eba50524095",
+    "url": "/ReactPortfolioTemplate/static/media/mock4.bd6a383d.png"
+  },
+  {
+    "revision": "aa1917bdce8260a1fb231e1c6c0953cf",
+    "url": "/ReactPortfolioTemplate/static/media/self.aa1917bd.png"
+  }
+]);
